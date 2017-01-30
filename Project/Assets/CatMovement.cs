26d3af57e6419d52769793a5ac0b3ced6c46ef4c
Unknown <nl1536@nyu.edu>
2017-01-30 18:13:19 -0500

    yjttyjtyjtj

---
+++ Project/Assets/CatMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatMovement : MonoBehaviour {
+
+    bool moveLeftRight;
+    float moveSpeedRight;
+    float moveSpeedLeft;
+
+	// Use this for initialization
+	void Start () {
+
+        moveSpeedRight = 0.15f;
+        moveSpeedLeft = 0.15f;
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when CAT hits BUILDING, can move left & right
+        if (GetComponent<Transform>().position.y <= 2.58f) {
+            moveLeftRight = true;
+        }
+        // when CAT hits right wall, can't move right
+        if (GetComponent<Transform>().position.x >= 7.19f) {
+            moveSpeedRight = 0;
+        }
+        // when CAT isn't against right wall, can move right
+        if (GetComponent<Transform>().position.x <= 7.19f)
+        {
+            moveSpeedRight = 0.15f;
+        }
+        // when CAT hits left wall, can't move left
+        if (GetComponent<Transform>().position.x <= -7.12) {
+            moveSpeedLeft = 0;
+        }
+        // when CAT isn't against left wall, can move left
+        if (GetComponent<Transform>().position.x >= -7.12)
+        {
+            moveSpeedLeft = 0.15f;
+        }
+        // CAT movement (left & right)
+        if (moveLeftRight == true) { 
+            // when LEFT ARROW pressed, CAT moves left
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - moveSpeedLeft,
+                                                                 GetComponent<Transform>().position.y,
+                                                                 GetComponent<Transform>().position.z);
+            }
+            // when RIGHT ARROW pressed, CAT moves right
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + moveSpeedRight,
+                                                                 GetComponent<Transform>().position.y,
+                                                                 GetComponent<Transform>().position.z);
+            }
+        }
+
+    }
+}
---
+++ Project/Assets/ManAutoRun.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManAutoRun : MonoBehaviour {
+
+    bool moveLeft;
+    bool moveRight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // when MAN hits GROUND, he starts moving left.
+        if (GetComponent<Transform>().position.y <= -3.650688) {
+            moveLeft = true;
+        }
+
+        // when MAN hits right side of screen, he starts moving left.
+        if (GetComponent<Transform>().position.x >= 8.82) {
+            moveRight = false;
+            moveLeft = true;
+        }
+
+        // when MAN hits left side of screen, he starts moving right.
+        if (GetComponent<Transform>().position.x <= -8.82) {
+            moveLeft = false;
+            moveRight = true;
+        }
+
+        // MAN move left
+        if (moveLeft == true) {
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - 0.1f,
+                                                             GetComponent<Transform>().position.y,
+                                                             GetComponent<Transform>().position.z);
+        }
+
+        // MAN move right
+        if (moveRight == true) {
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + 0.1f,
+                                                             GetComponent<Transform>().position.y,
+                                                             GetComponent<Transform>().position.z);
+        }
+	
+	}
+}
