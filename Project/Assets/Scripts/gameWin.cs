a87f8ac9eb949fc2a2751cd0a5a88ce332356d39
Still Noah <nl1536@nyu.edu>
2017-02-23 09:55:39 -0500

    added title screen and win screen kinda

---
+++ Project/Assets/Scripts/gameWin.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameWin : MonoBehaviour {
+
+    public bool win;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+        GameObject.Find("screenStart").GetComponent<startGame>().start = true;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D startScreenCollide) {
+        Physics2D.IgnoreLayerCollision(14, 8);
+        Physics2D.IgnoreLayerCollision(14, 9);
+        Physics2D.IgnoreLayerCollision(14, 10);
+        Physics2D.IgnoreLayerCollision(14, 11);
+        Physics2D.IgnoreLayerCollision(14, 12);
+        Physics2D.IgnoreLayerCollision(14, 13);
+        Physics2D.IgnoreLayerCollision(14, 15);
+    }
+
+    // INSERT WIN CONDITION HERE
+
+    void OnTriggerEnter2D(Collider2D startScreenTrigger) {
+        if (startScreenTrigger.gameObject.tag == "screenTrigger") {
+            win = false;
+        }
+    }
+}
---
+++ Project/Assets/Scripts/startGame.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame : MonoBehaviour {
+
+    public bool start;
+
+	// Use this for initialization
+	void Start () {
+
+        start = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown() {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D startScreenCollide) {
+        Physics2D.IgnoreLayerCollision(13, 8);
+        Physics2D.IgnoreLayerCollision(13, 9);
+        Physics2D.IgnoreLayerCollision(13, 10);
+        Physics2D.IgnoreLayerCollision(13, 11);
+        Physics2D.IgnoreLayerCollision(13, 12);
+        Physics2D.IgnoreLayerCollision(13, 14);
+        Physics2D.IgnoreLayerCollision(13, 15);
+
+        if (startScreenCollide.gameObject.tag == "screenCollider") {
+            start = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D startScreenTrigger) {
+        if (startScreenTrigger.gameObject.tag == "screenTrigger") {
+            start = false;
+        }
+    }
+}
